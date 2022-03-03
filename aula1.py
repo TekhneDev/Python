51d84c85ebc5bdefdff660566a060d12b6d0e9a7
Carla Mariana A. Braga <carlamab2808@outlook.com>
2022-03-03 01:55:23 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,22 @@
+#cometario
+print('luiz', 'otavio')
+print('luiz', 'otavio', sep='-',end='####')
+print('luiz', 'otavio', sep='-')
+
+#CPF imaginario
+print('233','456','177',sep='.',end="-")
+print('07')
+
+print('Essa é uma "string" (str).')
+print("Esse é meu \"texto\" (str).")
+print(r'Esse é meu \n (string)')
+
+print(type('carla'))
+print(bool(1))
+print('luiz', float('10'))
+print('23' + '45')
+
+print(19 > 18, type(32 > 18))
+
+# int + string = repetir a string tantas vezes
+print("Multiplicação *",10 * "Carla")
